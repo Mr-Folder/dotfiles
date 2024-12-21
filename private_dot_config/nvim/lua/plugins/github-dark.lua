a4f5ac7ee0be6b12beba9505f7919cbc5595fdec
mr-folder <melagomen@gmail.com>
2024-12-22 00:40:06 +0100

    change nvim theme

--- private_dot_config/nvim/lua/plugins/github-dark.lua
+++ private_dot_config/nvim/lua/plugins/github-dark.lua
@@ -3,22 +3,26 @@ return {
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
+    local options = {
+      transparent = true,
+    }
     local palettes = {
-      github_dark_high_contrast = {
+      all = {
         yellow = "#000000",
       },
     }
     local specs = {
-      github_dark_high_contrast = {
+      all = {
         syntax = {
           tag = "#ff9492",
           field = "#dbb7ff", -- Field
           builtin2 = "white", -- Builtin const
+          bg1 = "white",
         },
       },
     }
-    require("github-theme").setup({ palettes = palettes, specs = specs })
+    require("github-theme").setup({ palettes = palettes, specs = specs, options = options })
 
-    vim.cmd("colorscheme github_dark_high_contrast")
+    vim.cmd("colorscheme github_dark_default")
   end,
 }
