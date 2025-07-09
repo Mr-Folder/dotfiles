4e4330737c713876fb5b818a0b74acd3f3be5f23
Leonard PELTIER <melagomen@gmail.com>
2025-07-09 10:09:42 +0200

    nvim: add hardtime.nvim

---
+++ private_dot_config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,11 @@
+return {
+   "m4xshen/hardtime.nvim",
+   lazy = false,
+   dependencies = { "MunifTanjim/nui.nvim" },
+   opts = {
+    max_time = 2000,
+    max_count = 7,
+    disable_mouse = false,
+    restriction_mode = "hint"
+  },
+}
