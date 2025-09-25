c3def4a2aeba390848d1e5b9d96c0cf6b97b9803
Leonard Peltier <mister_folder@proton.me>
2025-09-25 14:52:03 +0200

    zsh: remove unused plugins

---
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  enabled = true,
+  opts = {
+    --   sections = {
+    --     lualine_a = {},
+    --     lualine_b = {},
+    --     lualine_c = {},
+    --     lualine_x = {},
+    --     lualine_y = {},
+    --     lualine_z = {},
+    --   },
+  },
+}
