bf384e700aa014eccf243232513bf8a4710c0602
Leonard Peltier <mister_folder@proton.me>
2025-11-09 16:58:52 +0100

    nvim: add cilium schemastore

--- private_dot_config/nvim/lua/plugins/lsp/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -43,7 +43,7 @@ return {
         -- lazy-load schemastore when needed
         on_new_config = function(new_config)
           new_config.settings.yaml.schemas =
-              vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
+            vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
         end,
         settings = {
           redhat = { telemetry = { enabled = false } },
@@ -53,14 +53,18 @@ return {
               enable = true,
             },
             validate = true,
+            -- Must disable built-in schemaStore support to use
+            -- schemas from SchemaStore.nvim plugin
+            enable = false,
+            -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+            url = "https://www.schemastore.org/api/json/catalog.json",
+          },
+          schemas = {
             schemaStore = {
-              -- Must disable built-in schemaStore support to use
-              -- schemas from SchemaStore.nvim plugin
-              enable = false,
-              -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-              url = "https://www.schemastore.org/api/json/catalog.json",
-            },
-            schemas = {
+              ["https://raw.githubusercontent.com/cilium/cilium/v1.18.3/install/kubernetes/cilium/values.schema.json"] = {
+                "**/cilium/**/values*.yml",
+                "**/cilium/**/values*.yaml",
+              },
               -- More specific Kubernetes schema patterns to avoid Helm templates
               kubernetes = {
                 "k8s/**/*.yaml",
@@ -74,24 +78,17 @@ return {
               },
               ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
               ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-              ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] =
-              "azure-pipelines.{yml,yaml}",
-              ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] =
-              "roles/tasks/*.{yml,yaml}",
-              ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] =
-              "*play*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json"] = "azure-pipelines.{yml,yaml}",
+              ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] = "roles/tasks/*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] = "*play*.{yml,yaml}",
               ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
               ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
               ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
               ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
-              ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] =
-              "*gitlab-ci*.{yml,yaml}",
-              ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] =
-              "*api*.{yml,yaml}",
-              ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
-              "*docker-compose*.{yml,yaml}",
-              ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] =
-              "*flow*.{yml,yaml}",
+              ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
             },
           },
         },
